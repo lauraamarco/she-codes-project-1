9de18d1f0197c3af1677158ea0cced755239500d
lauraamarco <lauraamarco22@gmail.com>
2024-02-17 19:41:35 +0100

    Upload Code from Codesandbox

---
+++ e2532ff3-63ec-47d2-a4e6-bf202ee23e99/src/index.js
@@ -0,0 +1,59 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
+function homework(event) {
+  let city = document.querySelector("#search-input");
+  let cityValue = city.value;
+  let apiKey = "db2eaa70afcd7t05847a43o4d14ba820";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}&units=metric`;
+
+  function challenge(response) {
+    let currentTemperature = document.querySelector(
+      "#current-temperature-degree"
+    );
+    currentTemperature.innerHTML = Math.round(
+      response.data.temperature.current
+    );
+  }
+  axios.get(apiUrl).then(challenge);
+}
+searchForm.addEventListener("submit", homework);
